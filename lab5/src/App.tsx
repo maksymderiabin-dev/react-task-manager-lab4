41a4c98a9f75e969bedf851259ba0e2eb81d13c7
Max <maksym.deriabin@kgemt.org.ua>
2026-06-10 01:05:52 +0300

    Lab 5: Initial Vite + React + TypeScript setup

---
+++ lab5/src/App.tsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <h1>Lab 5: Task API</h1>;
+}
---
+++ lab5/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './index.css'
+import App from './App.tsx'
+
+// Створюємо екземпляр QueryClient
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>,
+)
